d00626895fa028db83ebefece5f8c46b0f9fee9c
Grégory Betton <contact@grebett.com>
2020-01-01 13:06:21 +0100

    setup: renaming

--- webpack.config.js
+++ webpack.config.js
@@ -4,12 +4,9 @@ var autoprefixer = require('autoprefixer');
 var UnminifiedWebpackPlugin = require('unminified-webpack-plugin');
 
 module.exports = {
-  watch:   true,
-  context: __dirname + "/",
-  entry: [
-    __dirname + '/app/js/app.babel.jsx'
-    // 'webpack/hot/dev-server',
-  ],
+  watch: true,
+  context: __dirname + '/',
+  entry: [__dirname + '/app/js/app.jsx'],
   module: {
     preLoaders: [
       {
@@ -19,60 +16,59 @@ module.exports = {
     ],
     loaders: [
       { test: /\.(json)$/, exclude: /node_modules/, loaders: ['json-loader'] },
-      { test: /\.(jsx|.js|babel.jsx|babel.js)$/,
+      {
+        test: /\.(jsx|js|babel.jsx|babel.js)$/,
         exclude: /node_modules/,
-        loader:  'babel-loader'
+        loader: 'babel-loader'
       },
-      // {
-      //   test:   /\.tag$/,
-      //   loader: 'riotjs-loader',
-      //   type:   'babel',
-      //   exclude: /node_modules/
-      // },
-      // { test: /\.jade$/, loaders: ['jade'] },
-      { test: /\.(postcss.css)$/,  loader: "style-loader!css-loader!postcss-loader" },
+      {
+        test: /\.(postcss.css)$/,
+        loader: 'style-loader!css-loader!postcss-loader'
+      },
       { test: /\.html$/, loader: 'raw-loader' },
       {
         test: /\.(eot|woff|ttf|svg|png|jpg|wav|mp3)$/,
-        loader: 'url-loader?limit=30000&name=[name]-[hash].[ext]',
+        loader: 'url-loader?limit=30000&name=[name]-[hash].[ext]'
       }
     ]
   },
-  postcss: function () {
+  postcss: function() {
     return {
-      defaults: [ require('precss'), require('postcss-cssnext'), require('postcss-modules') ],
-      cleaner:  [autoprefixer({ browsers: ['last 2 versions'] })]
+      defaults: [
+        require('precss'),
+        require('postcss-cssnext'),
+        require('postcss-modules')
+      ],
+      cleaner: [autoprefixer({ browsers: ['last 2 versions'] })]
     };
   },
   output: {
-    path:           __dirname + '/app/build/',
-    filename:       'mojs-curve-editor.min.js',
-    publicPath:     'build/',
-    library:        'mojs-curve-editor',
-    libraryTarget:  'umd',
-    umdNamedDefine: true,
-    // hotUpdateChunkFilename: "[id].hot-update.js"
+    path: __dirname + '/app/build/',
+    filename: 'mojs-curve-editor.min.js',
+    publicPath: 'build/',
+    library: 'mojs-curve-editor',
+    libraryTarget: 'umd',
+    umdNamedDefine: true
   },
   plugins: [
-    // new webpack.ProvidePlugin({
-    //   h: 'preact'
-    // }),
-    // new webpack.NoErrorsPlugin(),
-    // new webpack.optimize.DedupePlugin(),
     new webpack.optimize.UglifyJsPlugin({
       compress: { warnings: false }
     }),
     new UnminifiedWebpackPlugin()
-    // new webpack.HotModuleReplacementPlugin()
   ],
-  // devtool: process.env.NODE_ENV==='production' ? 'source-map' : 'inline-source-map',
   resolve: {
-    root: [ path.resolve('./') ],
+    root: [path.resolve('./')],
     moduleDirectories: ['node_modules'],
     target: 'node',
     extensions: [
-      '', '.js', '.babel.js', '.babel.jsx',
-      '.postcss.css', '.css', '.json'
+      '',
+      '.js',
+      '.jsx',
+      '.babel.js',
+      '.babel.jsx',
+      '.postcss.css',
+      '.css',
+      '.json'
     ]
   }
 };
